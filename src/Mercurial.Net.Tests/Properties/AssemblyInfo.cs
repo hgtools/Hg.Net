7ed821c9ba5d19329f328763566b87430d36dd2b
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-12-26 17:52:10 +1000

    Cleanup AssemblyInfo files

--- src/Mercurial.Net.Tests/Properties/AssemblyInfo.cs
+++ src/Mercurial.Net.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
+using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
+
 [assembly: AssemblyTitle("Mercurial.Net.Tests")]
 [assembly: AssemblyDescription("Unit Tests for Mercurial.Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Lasse V. Karlsen")]
+[assembly: AssemblyCompany("vCipher")]
 [assembly: AssemblyProduct("Mercurial.Net")]
-[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © vCipher 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("e7e91ce0-7964-4296-a8fa-0a2c12ee2a0c")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: Guid("BFCEA5CD-31F1-47FF-8FD2-1CC032D0EE67")]
