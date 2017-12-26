8da08cdee0449f1827b07d20725cc048007f4077
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-12-26 15:37:10 +1000

    Moved from https://mercurialnet.codeplex.com

---
+++ Mercurial.Net.Tests/Hooks/CommitHookTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Mercurial.Tests.Hooks
+{
+    [TestFixture]
+    [Category("Integration")]
+    public class CommitHookTests : SingleRepositoryTestsBase
+    {
+        [Test]
+        public void Commit_SingleChangeset_OutputsThatChangeset()
+        {
+            Repo.Init();
+            Repo.SetHook("commit");
+
+            File.WriteAllText(Path.Combine(Repo.Path, "test.txt"), "dummy");
+            Repo.Add("test.txt");
+
+            var command = new CustomCommand("commit")
+                .WithAdditionalArgument("-m")
+                .WithAdditionalArgument("dummy");
+            Repo.Execute(command);
+
+            var tipHash = Repo.Tip().Hash;
+
+            Assert.That(command.RawExitCode, Is.EqualTo(0));
+            Assert.That(command.RawStandardOutput, Is.StringContaining("LeftParentRevision:0000000000000000000000000000000000000000"));
+            Assert.That(command.RawStandardOutput, Is.StringContaining("RightParentRevision:" + Environment.NewLine));
+            Assert.That(command.RawStandardOutput, Is.StringContaining("CommittedRevision:" + tipHash));
+        }
+    }
+}
---
+++ Mercurial.Net.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mercurial.Net.Tests")]
+[assembly: AssemblyDescription("Unit Tests for Mercurial.Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lasse V. Karlsen")]
+[assembly: AssemblyProduct("Mercurial.Net")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e7e91ce0-7964-4296-a8fa-0a2c12ee2a0c")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
